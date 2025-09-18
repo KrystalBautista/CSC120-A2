773b44fbda38c596f8def54472fd87d7605d4593
KrystalBautista <hbautista@smith.edu>
2025-09-17 23:06:54 -0400

    Completed A2

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# Import a few useful containers from the typing module
-from typing import Dict, Union
-
-# Import the functions we wrote in procedural_resale_shop.py
-from procedural_resale_shop import buy, update_price, sell, print_inventory, refurbish
+from computer import Computer
+from oo_resale_shop import ResaleShop
 
 """ This helper function takes in a bunch of information about a computer,
     and packages it up into a python dictionary to make it easier to store
@@ -28,51 +25,51 @@ def create_computer(description: str,
     }
 
 def main():
+    shop = ResaleShop()
     
-    # First, let's make a computer
-    computer = create_computer(
+    computer = Computer(
         "Mac Pro (Late 2013)",
         "3.5 GHc 6-Core Intel Xeon E5",
         1024, 64,
         "macOS Big Sur", 2013, 1500
     )
 
-    # Print a little banner
+    
     print("-" * 21)
     print("COMPUTER RESALE STORE")
     print("-" * 21)
 
-    # Add it to the resale store's inventory
-    print("Buying", computer["description"])
-    print("Adding to inventory...")
-    computer_id = buy(computer)
-    print("Done.\n")
+    
+    print("Buying", computer.description)
+    item_id = shop.buy(computer)
+    print()
 
-    # Make sure it worked by checking inventory
+    
     print("Checking inventory...")
-    print_inventory()
+    shop.print_inventory()
     print("Done.\n")
 
-    # Now, let's refurbish it
+    
     new_OS = "MacOS Monterey"
-    print("Refurbishing Item ID:", computer_id, ", updating OS to", new_OS)
-    print("Updating inventory...")
-    refurbish(computer_id, new_OS)
+    print(f"Refurbishing Item ID:{item_id}, updating OS to {new_OS}")
+    shop.refurbish(item_id, new_OS)
     print("Done.\n")
 
-    # Make sure it worked by checking inventory
+
     print("Checking inventory...")
-    print_inventory()
+    shop.print_inventory()
     print("Done.\n")
     
-    # Now, let's sell it!
-    print("Selling Item ID:", computer_id)
-    sell(computer_id)
     
-    # Make sure it worked by checking inventory
+    print(f"Selling Item ID: {item_id}")
+    shop.sell(item_id)
+    print()
+    
+    
     print("Checking inventory...")
-    print_inventory()
+    shop.print_inventory()
     print("Done.\n")
 
-# Calls the main() function when this file is run
-if __name__ == "__main__": main()
+
+if __name__ == "__main__":
+    main()
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,48 @@
+from computer import *
+
 class ResaleShop:
 
-    # What attributes will it need?
+    
+    def __init__(self):
+        self.inventory = []
+        self.next_id = 0
+        
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def buy(self, computer:Computer):
+        self.inventory.append(computer)
+        item_id = self.inventory.index(computer)
+        return item_id
 
-    # What methods will you need?
+
+    def sell(self, item_id: int):
+        if 0 <= item_id < len(self.inventory) and self.inventory[item_id] is not None:
+            self.inventory[item_id] = None
+            print(f"Item {item_id} sold!")
+        else:
+            print(f"Item {item_id} not found. Please select another item to sell.")    
+
+
+    def update_price(self, item_id:int, new_price:int):
+        if 0 <= item_id < len(self.inventory) and self.inventory[item_id] is not None:
+            self.inventory[item_id].update_price(new_price)
+        else:
+            print(f"Item {item_id} not found. Cannto update price.")
+
+
+
+
+    def refurbish(self, item_id:int, new_os:str = None):
+        if 0 <= item_id < len(self.inventory) and self.inventory [item_id] is not None:
+            computer = self.inventory[item_id]
+            computer.refurbish()
+            if new_os:
+                computer.update_os(new_os)
+        else:
+            print(f"Item {item_id} not found. Please select another item to refurbish.")
+
+    def print_inventory(self):
+        for item_id, computer in enumerate(self.inventory):
+            if computer is not None:
+                print(f"Item ID: {item_id} : {vars(computer)}")
+            else:
+                print(f"Item {item_id} sold!")
